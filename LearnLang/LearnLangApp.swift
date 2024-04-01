3909599aeb8610d30b5a7c946e6865901f478ddb
Alexey Kolch <alexeykolychenkov94@gmail.com>
2024-04-01 21:25:50 +0300

    Initial Commit

---
+++ LearnLang/LearnLangApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLangApp.swift
+//  LearnLang
+//
+//  Created by Алексей Колыченков on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LearnLangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
